602f69a4bc104adb3b5d7f0085fbc38c0e48ad5a
gunturmisbahsedjati <gunturmisbahedjati@gmail.com>
2024-01-26 09:21:42 +0700

    upload level 4

---
+++ src/services/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.contact-manager.project.skyshi.io";
+
+export async function getAllContactsData() {
+  return await axios.get(`${BASE_URL}/contacts`).catch((error) => {
+    return error;
+  });
+}
+
+export async function addNewContact(payload) {
+  return await axios.post(`${BASE_URL}/contacts`, payload).catch((error) => {
+    return error;
+  });
+}
+
+export async function deleteContact(id) {
+  return await axios.delete(`${BASE_URL}/contacts/${id}`).catch((error) => {
+    return error;
+  });
+}
+
+export async function updateContact(payload) {
+  return await axios.put(`${BASE_URL}/contacts/${payload.id}`, payload.data).catch((error) => {
+    return error;
+  });
+}
+
+// TODO:
+// 1. Buat sebuah fungsi untuk menghapus data kontak dengan mengirimkan id sebagai params dari kontak yang akan dihapus ke API menggunakan endpoint /contacts, axios method yang digunakan adalah delete
+// 2. Buat sebuah fungsi untuk mengubah data kontak dengan mengirimkan id sebagai params dari kontak yang akan diubah beserta dengan data perubahan terbaru dari kontak tersebut ke API menggunakan endpoint /contacts, axios method yang digunakan adalah put
